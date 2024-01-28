8ffa161cf27095148d0d05b86a02968417724782
Igor Dantas <113206684+igordants@users.noreply.github.com>
2024-01-28 20:43:16 -0300

    Criação e alteração da classe pessoa

--- projetojava1/src/curso/java/alex/pessoa.java
+++ projetojava1/src/curso/java/alex/pessoa.java
@@ -1,5 +1,23 @@
 package curso.java.alex;
 
 public class pessoa {
+			private String nome;
+			
+			public String getNome() {
+				return nome;
+			}
 
+			public void setNome(String nome) {
+				this.nome = nome;
+			}
+
+			public String getSobrenome() {
+				return sobrenome;
+			}
+
+			public void setSobrenome(String sobrenome) {
+				this.sobrenome = sobrenome;
+			}
+
+			private String sobrenome;
 }
